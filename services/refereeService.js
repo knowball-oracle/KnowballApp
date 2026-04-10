60f01c86ddae2c10bad3ee51f0c9aa8135281b5b
castropatrick <patrickcastro73@gmail.com>
2026-04-10 18:55:04 -0300

    Adicionando serviço de arbitragem.

---
+++ services/refereeService.js
@@ -0,0 +1,25 @@
+import api from "./api";
+
+export async function listarArbitros() {
+  const response = await api.get("/referees");
+  return response.data._embedded.refereeList;
+}
+
+export async function buscarArbitroPorId(id) {
+  const response = await api.get(`/referees/${id}`);
+  return response.data;
+}
+
+export async function criarArbitro(arbitro) {
+  const response = await api.post("/referees", arbitro);
+  return response.data;
+}
+
+export async function atualizarArbitro(id, arbitro) {
+  const response = await api.put(`/referees/${id}`, arbitro);
+  return response.data;
+}
+
+export async function excluirArbitro(id) {
+  await api.delete(`/referees/${id}`);
+}
