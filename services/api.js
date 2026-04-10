1fcfbc4e8d1dc357a485fedc2606aa77f3b6cd02
castropatrick <patrickcastro73@gmail.com>
2026-04-10 18:46:28 -0300

    Adicionando a API prévia

---
+++ services/api.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+//URL PREVIA ANTES DE TROCAR PARA O FLYWAY
+const BASE_URL = "http://localhost:8080";
+
+const api = axios.create({
+  baseURL: BASE_URL,
+});
+
+api.interceptors.request.use(async (config) => {
+  const token = await AsyncStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default api;
